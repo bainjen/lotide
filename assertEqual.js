2d3d1737831a060e5433a7af13a86525337edc12
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-08-17 23:50:51 +0000

    function to find if two inputs are equal

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5.5, 5.4);
+assertEqual('pippa', 'Pippa');
+assertEqual('Prairie', 'Prairie');
+// If the values match, it should print (console.log) the following: Assertion Passed: [actual] === [expected] (but with the values filled in)
+// Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
