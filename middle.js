2aff3a88ea3ed73d2e373496971e156237b97486
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-08-19 19:02:11 +0000

    function passes equal arrays test function

--- middle.js
+++ middle.js
@@ -44,6 +44,12 @@ const middle = function (array) {
   }
 }
 
+let arrayTest = [1, 3, 'six', 5, 7]
+assertArraysEqual(middle(arrayTest), ['six'])
+
+let arrayTest2 = [1, 3, 'six', 9, 5, 7]
+assertArraysEqual(middle(arrayTest2), ['six', 9])
+
 console.log(middle([1, 2, 8, 9, 5, 6, 2]));
 console.log(middle([1, 2, 8, 9, 5, 6]));
 console.log(middle([5, 6, 2]));
