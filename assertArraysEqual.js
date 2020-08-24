b5bc9d2a1c8c952db74d130966dee67ec4dd62cb
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-08-24 20:02:45 +0000

    rename files to match(was missing an s) and refactor code to import other fuctions

---
+++ assertArraysEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+
+
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅Assertion Passed: two arrays are equal.`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: two arrays are not equal.`);
+  }
+   
+};
+
+module.exports = assertArraysEqual
