75ffa832c5581fc9543db50bff9b3e638f60d4d5
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-08-24 19:53:49 +0000

    refactor eqArrays function to have separate test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,4 @@
-//++++INSTRUCTIONS++++++
-// Implement a function eqArrays which takes in two arrays and returns true or  false, based on a perfect match.
-// Use assertEqual to write test cases for various scenarios.
-
-//+++++EXAMPLE EXPECTED OUTCOME++++++++++++
-// eqArrays([1, 2, 3], [1, 2, 3]) // => true
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-//+++++++FUNCTIONS BELOW+++++++++++++++
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
 const eqArrays = function(arrayOne, arrayTwo) {
@@ -34,11 +14,5 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true);
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), false);
-assertEqual(eqArrays([1, 2, 3, 5], [1, 2, 3, 4]), false);
-assertEqual(eqArrays([1, 2, 3, 5], [1, 2, 3, 4]), true);
-
+module.exports = eqArrays; 
 
